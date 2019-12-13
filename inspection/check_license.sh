c13f68c06badc9f9378fbf9dbd144def3aeaef11
Dieter Pfeuffer <Dieter.Pfeuffer@men.de>
2019-12-13 16:20:33 +0100

    check_license.sh: skip pciutils, detection changed

--- inspection/check_license.sh
+++ inspection/check_license.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
 fail=0
+lic="www.gnu.org/licenses"
 
 # get total number of .c/.h files
 src=$(find . -type f \( -name "*.c" -or -name "*.h" \) | wc -l)
 
-# get number of .c/.h files without GNU license note
-srcwo=$(find . -type f \( -name "*.c" -or -name "*.h" \) -exec grep -HiRnL "GNU General Public License" {} \; | wc -l)
+# get .c/.h files without GNU license note
+files=$(find . -name pciutils -prune -o \( -name "*.c" -or -name "*.h" \) -exec grep -HiRL $lic {} \; )
 
-echo "$srcwo/$src .c/.h files without GNU license note"
+# count number of .c/.h files without GNU license note
+srcwo=$(echo $files | wc -w)
+
+echo "$srcwo/$src .c/.h files without GNU license note (pciutils dir skipped)"
 
 echo "list of affected files"
-find . -type f \( -name "*.c" -or -name "*.h" \) -exec grep -HiRnL "GNU General Public License" {} \;
+echo $files | tr " " "\n"
 
 # show result
 if [ $srcwo -gt 0 ]; then
